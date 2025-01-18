451847dc6c2576707adb310bedd97d00c1325909
Endem14 <mmerchan@Ualberta.ca>
2025-01-18 00:13:53 -0700

    Changes to meet our needs

---
+++ hackathon_job_dashboard/app/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+// Main APP entry point.
+export default function Main() {
+  return (
+    <SafeAreaView>
+      <Text>Root Layout</Text>
+    </SafeAreaView>
+  );
+}
